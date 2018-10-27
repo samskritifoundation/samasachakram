b3dc6470d7bb87caeed75f3137bc4b985bbf4c36
pankaja-shree <pankajashree@gmail.com>
2018-10-27 17:23:03 +0530

    add buttons for different topics

--- pages/math.vue
+++ pages/math.vue
@@ -2,36 +2,16 @@
   <v-layout column justify-center align-center class="papyrus">
     <v-flex xs12 sm8 md8>
       <div class="text-xs-center">
-<sflogo title="संस्कृति-प्रतिष्ठानम्" subtitle1="Samskriti Foundation" subtitle2="presents" />
+<sflogo title="संस्कृति-प्रतिष्ठानम्" subtitle1="Samskriti Foundation" />
       </div>
       <v-card class = "bordered text-xs-center" color="primary" dark>
-        <v-card-title class="myheader2 devanagari text-xs-center">वृत्तरत्नाकर-शिक्षकः - परस्परव्यवहारपूर्वकशिक्षणम्</v-card-title>
-        <v-card-title class="myheader2 papyrus text-xs-center">Interactive tutorial for Vrittaratnakara</v-card-title>
-        <v-card-text>
-          <p>Interactive self learning software for learning Sanskrit prosody, various types of meters used in poetry.</p>
-          <hr class="my-3">
-        </v-card-text>
+        <v-card-title class="myheader2 papyrus text-xs-center">Mathematics in Sanskrit Prosody</v-card-title>
         <v-card-actions>
-          <v-dialog v-model="dialog" scrollable>
-          <v-btn slot="activator" round color="accent" class="text-capitalize font-weight-bold" large :ripple="{ class: 'error--text' }"> About <br> Vrittaratnakara</v-btn>
-          <v-card color="info" class="papyrus bg1" light>
-            <v-card-title class="myheader papyrus">Vrittaratnakara</v-card-title>
-            <v-divider></v-divider>
-            <v-card-text class="subheading papyrus font-weight-bold">Vritta Ratnakara of Kedara Bhatta (14th Century CE) is one of
-the most popular texts on Sanskrit prosody. Though there are many books on Sanskrit prosody by eminent authors like Kalidasa, Kshemendra, etc, Vritta Ratankara continues to be an essential text for Sanskrit students. A speciality of this work is that the definition and illustration of a meter is given in one and the same verse. The verse defining a particular metre is composed in that particular meter itself. This is very helpful for a student of Sanskrit prosody. Another specialty of this work is that it is very brief – there are merely 136 verses. The author has covered all the prominent metres of Sanskrit literature in these verses.</v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="secondary" @click.native="dialog = false" ripple>Close</v-btn>
-            </v-card-actions>
-          </v-card>
-          </v-dialog>
-          <v-spacer></v-spacer>
-          <v-btn round color="accent" class="font-weight-bold text-capitalize" nuxt to="/lessons" large>अध्यायाः<br>Learn</v-btn>
+          <v-btn round color="accent" class="font-weight-bold text-capitalize" nuxt to="/" large>Binomial Theorem</v-btn>
            <v-spacer></v-spacer>
-          <v-btn round color="accent" class="font-weight-bold text-capitalize" nuxt to="/exercises" large>अभ्यासाः <br> Exercises</v-btn>
+          <v-btn round color="accent" class="font-weight-bold text-capitalize" nuxt to="/" large>Pascal's Triangle</v-btn>
           <v-spacer></v-spacer>
-          <v-btn round color="accent" class="font-weight-bold text-capitalize" nuxt to="/math" large>Mathematics in Sanskrit poetry</v-btn>
+          <v-btn round color="accent" class="font-weight-bold text-capitalize" nuxt to="/" large>Computer Applications</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
