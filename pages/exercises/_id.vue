5497d34f5a7af0dfa22e170903cfa141c9d869ac
pankaja-shree <pankajashree@gmail.com>
2018-11-02 15:45:55 +0530

    add sound effects

--- pages/exercises/_id.vue
+++ pages/exercises/_id.vue
@@ -43,7 +43,6 @@
             </v-alert>
             </v-flex>
           </v-layout>
-
   </v-card>
 </v-container>
 </template>
@@ -94,13 +93,18 @@ export default {
     },
     check (ans) {
       let correct = this.exercise.ans
+      // let correctAudio = new Audio('correct.mp3')
+      const sound = new Audio('correct.mp3')
+      let wrongAudio = new Audio('wrong.mp3')
       console.log(correct + ',' + this.chosen)
       if (this.chosen === correct) {
+        sound.play()
         this.success_alert = true
         this.fail_alert = false
         this.score = 1
         this.color = 'green'
       } else {
+        wrongAudio.play()
         this.success_alert = false
         this.fail_alert = true
         this.score = 0
