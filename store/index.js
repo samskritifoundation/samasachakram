6987fdc7eef584c07a3306ffbaeaae1b439458d6
pankaja-shree <pankajashree@gmail.com>
2018-12-10 16:12:22 +0530

    add sanskrit definitions and examples

--- store/index.js
+++ store/index.js
@@ -230,35 +230,280 @@ export const state = () => ({
       example_sans: ''
     },
     {
-      title_sans: '',
-      title_eng: ' ',
+      title_sans: 'तत्पुरुषः',
+      title_eng: 'Tatpuruṣaḥ',
       id: 5,
-      definition_sans: '',
-      definition_eng: '',
-      common: '',
-      example_sans: ''
+      definition_sans: 'उत्तरपदार्थप्रधानस्तत्पुरुषः ।',
+      definition_eng: 'This is the compound in which second object is more important.',
+      common: 'The type of Tatpuruṣa depends on the case suffix you have to append to the purvapada. ',
+      example_sans: '',
+      types: 8,
+      type: [
+        {
+          name: '1. प्रथमातत्पुरुषः (prathamātatpuruṣaḥ)',
+          sans: '',
+          ex: [
+            {
+              text: 'अर्ध पिप्पल्ल्याः अर्धपिप्पली । '
+            },
+            {
+              text: 'पूर्वं कायस्येति पूर्वकायः ।'
+            }
+          ]
+        },
+        {
+          name: '2. द्वितीयातत्पुरुषः (dvitīyātatpuruṣaḥ)',
+          ex: [
+            {
+              text: 'कृष्णं श्रितः कृष्णश्रितः ।'
+            },
+            {
+              text: ' ग्रामं गतो ग्रामगतः ।'
+            },
+            {
+              text: ' कान्तारमतीतः कान्तारातीतः ।'
+            }
+          ]
+        },
+        {
+          name: '3. तृतीयातत्पुरुषः (tṛtīyātatpuruṣaḥ)',
+          ex: [
+            {
+              text: 'शङ्कुलया खण्डः शङ्कुलाखंडः।'
+            },
+            {
+              text: ' धान्येनार्थो धान्यार्थः ।'
+            },
+            {
+              text: ' मासेन पूर्वो मासपूर्वः ।'
+            }
+          ]
+        },
+        {
+          name: '4. चतुर्थीतत्पुरुषः (caturthītatpuruṣaḥ)',
+          ex: [
+            {
+              text: 'यूपाय दारु यूपदारु ।'
+            },
+            {
+              text: ' कुण्डलाय हिरण्यं कुण्डलहिरण्यम् ।'
+            },
+            {
+              text: ' गुरवे दक्षिणा गुरुदक्षिणा ।'
+            }
+          ]
+        },
+        {
+          name: '5. पञ्चमीतत्पुरुषः (pañcamītatpuruṣaḥ)',
+          ex: [
+            {
+              text: 'अर्थात् अपेतः अर्थापेतः । '
+            },
+            {
+              text: 'सिंहात्‌ भयम्‌ सिंहभयम्‌।'
+            },
+            {
+              text: ' वृश्चिकात्‌ भीः वृश्चिकभीः ।'
+            }
+          ]
+        },
+        {
+          name: '6. षष्ठीतत्पुरुषः (ṣaṣṭhītatpuruṣaḥ)',
+          ex: [
+            {
+              text: 'कृष्णस्य भक्तः कृष्णभक्तः ।'
+            },
+            {
+              text: ' आम्रस्य फलम् आम्रफलम् ।'
+            },
+            {
+              text: ' राज्ञः पुरुषो राजपुरुषः ।'
+            }
+          ]
+        },
+        {
+          name: '7. सप्तमीतत्पुरुषः (saptamītatpuruṣaḥ)',
+          ex: [
+            {
+              text: 'अक्षेषु शौण्डः अक्षशौण्डः ।'
+            },
+            {
+              text: ' कर्मणि कुशलः कर्मकुशलः ।'
+            },
+            {
+              text: ' विद्यायां निपुणः विद्यानिपुणः ।'
+            }
+          ]
+        },
+        {
+          name: '8. नञ् तत्पुरुषः (nañ tatpuruṣaḥ)',
+          explanation: 'The words having prefixes अ and अन्‌ in negation are called nañ tatpuruṣaḥ',
+          ex: [
+            {
+              text: 'न ब्राह्मणः अब्राह्मणः ।'
+            },
+            {
+              text: ' न वृषभः अवृषभः ।'
+            },
+            {
+              text: ' पापाभावः अपापम्  ।'
+            },
+            {
+              text: 'धर्मविरुद्धोऽधर्मः ।'
+            }
+          ]
+        }
+      ]
     },
     {
-      title_sans: '',
-      title_eng: ' ',
+      title_sans: 'कर्मधारयः',
+      title_eng: 'Karmadhārayaḥ',
       id: 6,
       definition_sans: '',
       definition_eng: '',
       common: '',
-      example_sans: ''
+      types: 7,
+      type: [
+        {
+          name: '1. विशेषणपूर्वपदः (viśeṣaṇapūrvapadaḥ)',
+          explanation: 'the first word is adjective',
+          ex: [
+            {
+              text: 'कृष्णश्चाऽसौ सर्पश्च कृष्णसर्पः । <br>कृष्णौ च तौ सर्पौ च कृष्णसर्पौ ।<br> कृष्णाश्च ते सर्पाश्च कृष्णसर्पाः ।'
+            },
+            {
+              text: ' रक्ता चासौ लता च रक्तलिता । <br>रक्ते च ते लते च रक्तलते । <br>रक्ताश्च ताः लताश्च रक्तलताः । '
+            },
+            {
+              text: 'नीलं च तत् उत्पलं च नीलोत्पलम् । <br>नीले च ते उत्पले च नीलोत्पले । <br>नीलानि च तानि उत्पलानि च नीलोत्पलानि ।'
+            }
+          ]
+        },
+        {
+          name: '2. विशेष्यपूर्वपदः (viśeṣyapūrvapadaḥ) ',
+          explanation: 'the first word is viśeṣya – the one to which adjective is to be appended',
+          ex: [
+            {
+              text: 'वैयाकरणश्चासौ खसूचिश्च वैयाकरणखसूचिः ॥'
+            },
+            {
+              text: ' गोपालश्चासौ बालश्च गोपालबालः ।'
+            }
+          ]
+        },
+        {
+          name: '3. विशेषणोभयपदः (viśeṣaṇobhayapadaḥ) ',
+          explanation: 'both the words are adjectives',
+          ex: [
+            {
+              text: 'शीतं च तत्  उष्णं च शीतोष्णम् ।'
+            }
+          ]
+        },
+        {
+          name: '4. उपमानपूर्वपदः (upamānapūrvapadaḥ) ',
+          explanation: 'The first word is upamāna – the thing to which comparision is made',
+          ex: [
+            {
+              text: 'मेघ इव स्यामो मेघश्यामः ।'
+            },
+            {
+              text: ' कम्बुवत् ग्रीवा कम्बुग्रीवा ।'
+            },
+            {
+              text: 'चन्द्रवत् मुखं चन्द्रमुखम् ।'
+            }
+          ]
+        },
+        {
+          name: '5. उपमानोत्तरपदः (upamānottarapadaḥ) ',
+          explanation: 'The second word is upamāna',
+          ex: [
+            {
+              text: 'पुरुषः व्याघ्र इव पुरुषव्याघ्रः ।'
+            },
+            {
+              text: ' नरः सिंह इव नरसिंहः ।'
+            }
+          ]
+        },
+        {
+          name: '6. सम्भावनापूर्वपदः (sambhāvanāpūrvapadaḥ)',
+          explanation: 'The first word is sambhāvanā, joined by iti',
+          ex: [
+            {
+              text: 'गुण इति बुद्धिः गुणबुद्धिः '
+            }
+          ]
+        },
+        {
+          name: '7. अवधारणापूर्वपदः (avadhāraṇāpūrvapadaḥ) ',
+          explanation: 'the first word is avadhāraṇā, joined by eva',
+          ex: [
+            {
+              text: 'विद्यैव धनं विद्याधनम् ॥'
+            },
+            {
+              text: ' अविद्यैव शृङ्खला अविद्याशृंखला ।'
+            }
+          ]
+        },
+        {
+          name: '8. मध्यमपदलोपी समास',
+          explanation: 'One or more word has to be imported according to context to fulfil the meaning of the samāsa',
+          ex: [
+            {
+              text: 'शाकप्रियः पार्थिवः शाकपार्थीवः ।'
+            },
+            {
+              text: ' देवपूजको ब्राह्मणः देवब्राह्मणः ।'
+            }
+          ]
+        }
+      ]
     },
     {
-      title_sans: '',
-      title_eng: ' ',
+      title_sans: 'बहुव्रीहिः',
+      title_eng: 'Bahuvrīhi',
       id: 7,
       definition_sans: '',
       definition_eng: '',
       common: '',
-      example_sans: ''
+      types: 7,
+      type: [
+        {
+          name: '1. द्विपदः (dvipadaḥ) ',
+          link: '/4'
+        },
+        {
+          name: '2. बहुपदः (bahupadaḥ) ',
+          link: '/4'
+        },
+        {
+          name: '3. सहपूर्वपदः (sahapūrvapadaḥ) ',
+          link: '/4'
+        },
+        {
+          name: '4. संख्योत्तरपदः (saṃkhyottarapadaḥ) ',
+          link: '/4'
+        },
+        {
+          name: '5. संख्योभयपदाः (saṃkhyobhayapadāḥ) ',
+          link: '/4'
+        },
+        {
+          name: '6. व्यतिहारलक्षणः (vyatihāralakṣaṇaḥ) ',
+          link: '/4'
+        },
+        {
+          name: '7. दिगन्तराललक्षणः (digantarālalakṣaṇaḥ) ',
+          link: '/4'
+        }
+      ]
     },
     {
-      title_sans: '',
-      title_eng: ' ',
+      title_sans: 'द्विगुः',
+      title_eng: 'Dvigu',
       id: 8,
       definition_sans: '',
       definition_eng: '',
@@ -266,8 +511,8 @@ export const state = () => ({
       example_sans: ''
     },
     {
-      title_sans: '',
-      title_eng: ' ',
+      title_sans: 'द्वन्द्वः',
+      title_eng: 'Dvandva',
       id: 9,
       definition_sans: '',
       definition_eng: '',
@@ -275,8 +520,8 @@ export const state = () => ({
       example_sans: ''
     },
     {
-      title_sans: '',
-      title_eng: ' ',
+      title_sans: 'अव्ययीभावः',
+      title_eng: 'Avyayībhāva',
       id: 10,
       definition_sans: '',
       definition_eng: '',
