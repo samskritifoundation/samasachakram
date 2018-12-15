c90a0f54e82e831ac22f1d0985e7a684fa0c73ab
pankaja-shree <pankajashree@gmail.com>
2018-12-15 16:01:01 +0530

    add source for english translation

--- pages/credits.vue
+++ pages/credits.vue
@@ -7,7 +7,7 @@
     <v-card color="transparent" flat>
       <v-card-text class="subheading font-weight-black text-xs-center">
       <h3>Credits</h3>
-      <p>English translations are based on the translation available <a href="https://www.sanskritworld.in/pages/detail/pagename/samasa">here</a></p>
+      <p>English translations are based on the translation available <a href="https://www.sanskritworld.in/pages/detail/pagename/samasa">here</a> and <a href="https://en.wikipedia.org/wiki/Sanskrit_compound#Aluk-sam%C4%81sa">here</a></p>
     </v-card-text>
     </v-card>
     </v-flex>
