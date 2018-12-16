cdbfe090b51aeb6bf6b50bbd124738db61d5d1b9
pankaja-shree <pankajashree@gmail.com>
2018-12-16 15:47:16 +0530

    change title

--- pages/exercises/index.vue
+++ pages/exercises/index.vue
@@ -2,7 +2,7 @@
   <v-container class="mt-0 pt-0">
   <v-card color="accent lighten-4" class="bordered papyrus mt-0 pt-0">
     <v-card-title primary-title>
-      <h3 class="papyrus myheader">Vrittaratnakara Exercises</h3>
+      <h3 class="papyrus myheader">Samāsacakra Exercises</h3>
       </v-card-title>
       <v-btn to="/exercises/1" large color="secondary" class="text-capitalize font-weight-bold" round> Start exercises</v-btn>
   </v-card>
